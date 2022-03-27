2cb3a808e678b76d191a5b56049bc163255bc782
Augusto Villar <augusto.villar@usp.br>
2022-03-27 20:35:50 -0300

    Co-authored-by: vinibalvarenga <vinibalvarenga@users.noreply.github.com>

---
+++ ponto_flutuante.asm
@@ -0,0 +1,38 @@
+ @ /0000
+;Optamos por colocar o valor de C igual a 11 decimal, 
+;por termos escolhido o padrao binary64 (double precision)
+;
+;
+CONVERTIDO1 K ;s = 0000 ou 0001
+            K ; 0000 < c <  2047(07FF)
+            K ;f (16 bits) mais significativo para menos significativo
+            K ;f (16 bits)
+            K ;f (16 bits)
+            K ;f (4 bits)
+;  
+CONVERTIDO2 K ; s
+            K ; c
+            K ; f
+            K ; f
+            K ; f
+            K ; f
+
+
+VARIAVEL1 K /0000
+
+VARIAVEL2 K /0000
+2929922992292929.35985555555555555544454654654
+
+3239
+
+29
+
+FLOAT SC
+      
+      
+      
+      RS     
+
+
+
+# MAIN
