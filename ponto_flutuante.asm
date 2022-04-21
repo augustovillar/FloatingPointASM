4fbf08aec942c38e4189106bab6524003df5464a
Augusto Villar <augusto.villar@usp.br>
2022-04-21 18:23:27 -0300

    Co-authored-by: Vinícius <vinibalvarenga@users.noreply.github.com>

--- ponto_flutuante.asm
+++ ponto_flutuante.asm
@@ -1,38 +1,94 @@
  @ /0000
 ;Optamos por colocar o valor de C igual a 11 decimal, 
-;por termos escolhido o padrao binary64 (double precision)
 ;
 ;
-CONVERTIDO1 K ;s = 0000 ou 0001
-            K ; 0000 < c <  2047(07FF)
-            K ;f (16 bits) mais significativo para menos significativo
-            K ;f (16 bits)
-            K ;f (16 bits)
-            K ;f (4 bits)
+
+SIGNAL         K /0000; s = 0000 ou 0001
+CARACTERISTICA K #00000; 0000 < c <  31 
+MANTISSA       K /0000; f (16 bits) mais significativo para menos significativo
 ;  
-CONVERTIDO2 K ; s
-            K ; c
-            K ; f
-            K ; f
-            K ; f
-            K ; f
 
 
-VARIAVEL1 K /0000
+VARIAVEL K /0000 ;numero a ser convertido
 
-VARIAVEL2 K /0000
-2929922992292929.35985555555555555544454654654
-
-3239
-
-29
-
-FLOAT SC
-      
-      
-      
-      RS     
+FLOAT        K /0000;
+            LD VARIAVEL;primeiro testa o s e o coloca na calor
+            JN NEGATIVO;
+            LV /0000 ;carrega 0
+            MM SIGNAL ;
+            JP VOLTASIGNAL
+NEGATIVO    LV /0001 ; garrega um 
+            MM SIGNAL ; 
+            JP VOLTASIGNAL           
+VOLTASIGNAL
 
 
-laksla
+
+
+
+
+
+      RS FLOAT; retorna a funcao
+
+variavel k /1234
+
+
+BINARIO1 K /0000;
+
+CONST1  K /1000;
+CONST2  K /0100;
+CONST3  K /0010;
+DOIS    K /0002;
+MENOSUM K /FFFF;
+
+INTERMEDIARIO K /0009;
+
+HEX2BIN K /0000; guarda endereco
+
+       LD INTERMEDIARIO;
+       DV DOIS; 0004
+       ML DOIS; 0008
+       SB INTERMEDIARIO; -0001
+       ML MENOSUM; 0001
+       MM BINARIO1; 
+
+       LD INTERMEDIARIO;
+       DV DOIS; 0004
+       MM INTERMEDIARIO; 0004
+       DV DOIS; 0002
+       ML DOIS; 0004
+       SB INTERMEDIARIO; 0000
+       ML MENOSUM;
+       ML CONST3; 0000
+       AD BINARIO1;
+       MM BINARIO1; 
+
+       LD INTERMEDIARIO; 0004
+       DV DOIS; 0002
+       MM INTERMEDIARIO; 0002
+       DV DOIS; 0001
+       ML DOIS; 0002
+       SB INTERMEDIARIO; 0000
+       ML MENOSUM;
+       ML CONST2; 
+       AD BINARIO1;
+       MM BINARIO1;
+
+
+       LD INTERMEDIARIO; 0002
+       DV DOIS; 0001
+       MM INTERMEDIARIO; 0001
+       DV DOIS; 0000
+       ML DOIS; 0000
+       SB INTERMEDIARIO; -0001
+       ML MENOSUM;0001
+       ML CONST1; 
+       AD BINARIO1;
+       MM BINARIO1;
+
+       LD BINARIO1;
+
+       RS HEX2BIN; retorna
+
+;
 # MAIN
