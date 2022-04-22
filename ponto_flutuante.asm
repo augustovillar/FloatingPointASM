ca433d4ae84e481a3bb31a36c1db73b8eea2f4a9
Augusto Villar <augusto.villar@usp.br>
2022-04-22 09:36:12 -0300

    main

--- ponto_flutuante.asm
+++ ponto_flutuante.asm
@@ -8,12 +8,22 @@ CARACTERISTICA K #00000; 0000 < c <  31
 MANTISSA1      K /0000; 
 MANTISSA2      K /0000;
 MANTISSA3      K /0000;
-MANTISSA3      K /0000;
-f (16 bits) mais significativo para menos significativo
-;  
+MANTISSA4      K /0000;
+; f (16 bits) mais significativo para menos significativo
 
 
 VARIAVEL K /0000 ;numero a ser convertido
+
+BINARIO1    K /0000
+BINARIO2    K /0000
+BINARIO3    K /0000
+BINARIO4    K /0000
+
+;constantes e varivaies para a funcao float
+
+
+
+
 
 FLOAT        K /0000;
             LD VARIAVEL;primeiro testa o s e o coloca na calor
@@ -24,21 +34,45 @@ FLOAT        K /0000;
 NEGATIVO    LV /0001 ; garrega um 
             MM SIGNAL ; 
             JP VOLTASIGNAL 
-;depois de descobrir o sinal, precisamos transformartransformar a varivel em bits          
-VOLTASIGNAL 
+;depois de descobrir o sinal, precisamos transformartransformar a varivel em bits, guardando nas mantissas          
+VOLTASIGNAL LD VARIAVEL;1234
+            ML CONST1;4000
+            DV CONST1;0004
+            MM INTERMEDIARIO;
+            SC HEX2BIN;
+            MM BINARIO4;
+
+            LD VARIAVEL; 1234
+            ML CONST2; 3400
+            DV CONST1; 0003
+            MM INTERMEDIARIO;
+            SC HEX2BIN;
+            MM BINARIO3;
+
+            LD VARIAVEL; 1234
+            ML CONST3; 2340
+            DV CONST1; 0002
+            MM INTERMEDIARIO;
+            SC HEX2BIN;
+            MM BINARIO2;
+
+            LD VARIAVEL; 1234
+            DV CONST1; 0001
+            MM INTERMEDIARIO;
+            SC HEX2BIN;
+            MM BINARIO1;
+
+            SC ENCONTRAUM; funcao irá procurar em qual posicao o primeiro 1 se encontra, na varivel binarios
 
 
 
 
+            RS FLOAT; retorna a funcao
 
 
 
-      RS FLOAT; retorna a funcao
-
-variavel k /1234
-
-
-BINARIO1 K /0000;
+;variaveis para a funcao HEX2BIN
+BINARIO K /0000;
 
 CONST1  K /1000;
 CONST2  K /0100;
@@ -46,7 +80,7 @@ CONST3  K /0010;
 DOIS    K /0002;
 MENOSUM K /FFFF;
 
-INTERMEDIARIO K /0009;
+INTERMEDIARIO K /0000;
 
 HEX2BIN K /0000; guarda endereco
 
@@ -55,7 +89,7 @@ HEX2BIN K /0000; guarda endereco
        ML DOIS; 0008
        SB INTERMEDIARIO; -0001
        ML MENOSUM; 0001
-       MM BINARIO1; 
+       MM BINARIO; 
 
        LD INTERMEDIARIO;
        DV DOIS; 0004
@@ -65,8 +99,8 @@ HEX2BIN K /0000; guarda endereco
        SB INTERMEDIARIO; 0000
        ML MENOSUM;
        ML CONST3; 0000
-       AD BINARIO1;
-       MM BINARIO1; 
+       AD BINARIO;
+       MM BINARIO; 
 
        LD INTERMEDIARIO; 0004
        DV DOIS; 0002
@@ -76,8 +110,8 @@ HEX2BIN K /0000; guarda endereco
        SB INTERMEDIARIO; 0000
        ML MENOSUM;
        ML CONST2; 
-       AD BINARIO1;
-       MM BINARIO1;
+       AD BINARIO;
+       MM BINARIO;
 
 
        LD INTERMEDIARIO; 0002
@@ -88,12 +122,21 @@ HEX2BIN K /0000; guarda endereco
        SB INTERMEDIARIO; -0001
        ML MENOSUM;0001
        ML CONST1; 
-       AD BINARIO1;
-       MM BINARIO1;
+       AD BINARIO;
+       MM BINARIO;
 
-       LD BINARIO1;
+       LD BINARIO;
 
        RS HEX2BIN; retorna
 
-;
+ZERO K /0000;
+UM K /0001;
+CONTADOR K /0000;
+
+ENCONTRAUM  K /0000; 
+
+            ;verifica a MANTISSA1
+
+
+
 # MAIN
